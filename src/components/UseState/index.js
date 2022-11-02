7a2a3936f72eefe0a79d8bb1936939852a92087b
HeyNow <app_academy_student_bk@protonmail.com>
2022-11-02 02:59:30 +0000

    Update package-lock.json and index.js

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import './UseState.css';
+
 const UseState = () => {
+  const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState(0);
+  
   return (
-    <div>
+    <div className={`${theme} state`}>
       <h1>UseState Component</h1>
+      <button onClick={() => setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light')}>Toggle Theme</button>
+      <h1>{count}</h1>
+      <br/>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increment</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>Decrement</button>
     </div>
   );
 };
